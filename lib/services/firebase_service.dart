8cef6dc1ee05cd3c9d86a309a2c9b4c89edd52b3
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-05-19 13:23:52 +0545

    adding firebase service

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,5 +1,16 @@
 // creating firebaseService class
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 class FirebaseService {
+//creating a variable called firebase Auth
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  FirebaseStorage _db = FirebaseStorage.instance;
+  FirebaseFirestore _storage = FirebaseFirestore.instance;
+
 // creating FirebaseService constructor
 
   FirebaseService();
