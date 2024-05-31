9aabe5211f7d6770bd870455b0b11e29cb9841a5
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-05-31 20:09:58 +0545

    userlogin using firebase

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,168 +1,7 @@
-// import 'package:flutter/material.dart';
-
-// class LoginPage extends StatefulWidget {
-//   const LoginPage({super.key});
-
-//   @override
-//   State<LoginPage> createState() => _LoginPageState();
-// }
-
-// class _LoginPageState extends State<LoginPage> {
-//   double? _deviceHeight, _deviceWidth;
-//   final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
-//   bool passwordObscured = true;
-//   TextEditingController email = TextEditingController();
-//   TextEditingController password = TextEditingController();
-//   bool isLoading = false;
-//   String? _email;
-//   String? _password;
-//   @override
-//   Widget build(BuildContext context) {
-//     _deviceHeight = MediaQuery.of(context).size.height;
-//     _deviceWidth = MediaQuery.of(context).size.width;
-
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Container(
-//           padding: EdgeInsets.symmetric(
-//             horizontal: _deviceWidth! * 0.05,
-//           ),
-//           child: Center(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.spaceAround,
-//               mainAxisSize: MainAxisSize.max,
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 _titleWidget(),
-//                 _loginForm(),
-//                 _loginButton(),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _titleWidget() {
-//     return const Text(
-//       "Finstagram",
-//       style: TextStyle(
-//         color: Colors.black,
-//         fontSize: 25,
-//         fontWeight: FontWeight.w600,
-//       ),
-//     );
-//   }
-
-//   Widget _loginButton() {
-//     return MaterialButton(
-//       onPressed: _loginUser,
-//       minWidth: _deviceWidth! * 0.70,
-//       height: _deviceHeight! * 0.06,
-//       color: Colors.red,
-//       child: const Text(
-//         "Login",
-//         style: TextStyle(
-//             color: Colors.white, fontSize: 25, fontWeight: FontWeight.w600),
-//       ),
-//     );
-//   }
-
-//   Widget _loginForm() {
-//     return Container(
-//       height: _deviceHeight! * 0.70,
-//       child: Form(
-//         key: _loginFormKey,
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           mainAxisSize: MainAxisSize.max,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             _emailTextField(),
-//             SizedBox(height: 40),
-//             _passwordTextField(),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _emailTextField() {
-//     return TextFormField(
-//       controller: email,
-//       decoration: const InputDecoration(
-//         hintText: "Email",
-//         prefixIcon: Icon(
-//           Icons.email,
-//         ),
-//       ),
-//       onSaved: (_value) {
-//         setState(() {
-//           _email = _value;
-//         });
-//       },
-//       validator: (_value) {
-//         bool _result = _value!.contains(
-//           RegExp(r"/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/"),
-//         );
-
-//         return _result ? null : "Please enter  a valid email";
-//       },
-//     );
-//   }
-
-//   Widget _passwordTextField() {
-//     return TextFormField(
-//       obscureText: passwordObscured,
-//       controller: password,
-//       //  obscureText: true, // Hide password characters
-//       decoration: InputDecoration(
-//         hintText: " Password",
-//         prefixIcon: const Icon(Icons.lock),
-//         suffixIcon: IconButton(
-//           onPressed: () {
-//             setState(() {
-//               passwordObscured = !passwordObscured;
-//             });
-//           },
-//           icon: Icon(
-//             passwordObscured ? Icons.visibility_off : Icons.visibility,
-//           ),
-//         ),
-//       ),
-
-//       onSaved: (_value) {
-//         setState(() {
-//           _password = _value;
-//         },);
-
-//       },
-//         validator:
-//         (_value) => _value!.length > 6
-//             ? null
-//             : "Please enter a password greater than 6  characters";
-//         // (_value) {
-//         //   if (_value == null || _value.isEmpty) {
-//         //     return "Please enter a password";
-//         //   } else if (_value.length < 6) {
-//         //     return "Password must be atlease 6 character long";
-//         //   }
-//         //   return _value;
-//         // };
-
-//     );
-//   }
-
-//   void _loginUser() {
-//     print(_loginFormKey.currentState!.validate());
-//     if (_loginFormKey.currentState!.validate()) {}
-//   }
-// }
-
 // ignore_for_file: unused_field
-
+import 'package:finstagram/services/firebase_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -173,6 +12,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   double? _deviceHeight, _deviceWidth;
+  FirebaseService? _firebaseService;
+
   final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
   bool passwordObscured = true;
   TextEditingController email = TextEditingController();
@@ -180,6 +21,12 @@ class _LoginPageState extends State<LoginPage> {
   bool isLoading = false;
   String? _email;
   String? _password;
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseService = GetIt.instance.get<FirebaseService>();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -307,11 +154,15 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  void _loginUser() {
+  void _loginUser() async {
     print(_loginFormKey.currentState!.validate());
     //
     if (_loginFormKey.currentState!.validate()) {
       _loginFormKey.currentState!.save();
+
+      bool _result = await _firebaseService!
+          .loginUser(email: _email!, password: _password!);
+      if (_result) Navigator.popAndPushNamed(context, 'home');
     }
   }
 
--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -5,6 +5,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+final String USER_COLLECTION = 'users';
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
 class FirebaseService {
 //creating a variable called firebase Auth
 
@@ -14,7 +17,7 @@ class FirebaseService {
   FirebaseFirestore _storage = FirebaseFirestore.instance;
 
 // creating FirebaseService constructor
-
+  Map? currentUser;
   FirebaseService();
 
   Future<bool> loginUser(
@@ -22,6 +25,8 @@ class FirebaseService {
     try {
       UserCredential _userCredential = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
+
+      currentUser = await getUserData(uid: _userCredential.user!.uid);
       if (_userCredential.user != null) {
         return true;
       } else {
@@ -31,5 +36,11 @@ class FirebaseService {
       print(e);
       return false;
     }
+  }
+
+  Future<Map> getUserData({required String uid}) async {
+    DocumentSnapshot _doc =
+        await _firestore.collection(USER_COLLECTION).doc(uid).get();
+    return _doc.data() as Map;
   }
 }
