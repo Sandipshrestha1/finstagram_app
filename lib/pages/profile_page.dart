7fb29cccd4ebed21ff00fb7d014ada080b918ec9
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-06-07 10:08:06 +0545

    Displaying user profile image

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:finstagram/services/firebase_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,8 +10,44 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  double? _deviceHeight, _deviceWidth;
+  FirebaseService? _firebaseService;
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseService = GetIt.instance.get<FirebaseService>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth! * 0.05,
+        vertical: _deviceHeight! * 0.02,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [],
+      ),
+    );
+  }
+
+  Widget _profileImage() {
+    return Container(
+      height: _deviceHeight! * 0.15,
+      width: _deviceHeight! * 0.15,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: NetworkImage(
+          _firebaseService!.currentUser!["image"],
+        ),
+      )),
+    );
   }
 }
